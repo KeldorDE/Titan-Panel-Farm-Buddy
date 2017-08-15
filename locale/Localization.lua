ad163127881c819e77f7a763a324177c10fe26bb
André Evertz <andre.evertz@devil-coding.de>
2017-08-15 21:07:26 +0200

    Updated locales

--- locale/Localization.lua
+++ locale/Localization.lua
@@ -1,11 +1,11 @@
 local L = LibStub('AceLocale-3.0'):NewLocale('Titan', 'enUS', true)
 
-L["An number for your farming goal"] = "An number for your farming goal"
+L["An number for your farming goal."] = true
 L["Author"] = true
-L["Enter the name of an item or CTRL + Click an item from your inventory"] = true
+L["Enter the name of an item or CTRL + Click an item from your inventory."] = true
 L["General Options"] = true
 L["Goal Quantity"] = true
 L["Item to Count"] = true
-L["The goal quantity for the tracked item"] = true
+L["The goal quantity for the tracked item."] = true
 L["The name of the item to track"] = true
 L["Version"] = true
