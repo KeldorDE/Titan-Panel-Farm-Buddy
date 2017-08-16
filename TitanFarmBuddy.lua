4f2fbe318d00e35456f7f4bb01055fd9e20b30ce
André Evertz <evertz@publicplan.de>
2017-08-16 17:26:20 +0200

    Added Libs Code refactoring Added new options with AceConfig

--- TitanFarmBuddy.lua
+++ TitanFarmBuddy.lua
@@ -4,107 +4,19 @@
 -- * By: Keldor
 -- **************************************************************************
 
-local L = LibStub('AceLocale-3.0'):GetLocale('Titan', true)
 local TITAN_FARM_BUDDY_ID = 'FarmBuddy'
-local TitanFarmBuddy = LibStub('AceAddon-3.0'):NewAddon(TITAN_FARM_BUDDY_ID, "AceConsole-3.0")
 local ADDON_NAME = 'Titan Farm Buddy'
+local L = LibStub('AceLocale-3.0'):GetLocale('Titan', true)
+local TitanFarmBuddy = LibStub('AceAddon-3.0'):NewAddon(TITAN_FARM_BUDDY_ID, 'AceConsole-3.0')
 local ADDON_VERSION = GetAddOnMetadata('TitanFarmBuddy', 'Version')
-local ADDON_AUTHOR = GetAddOnMetadata('TitanFarmBuddy', 'Author')
-
-local defaults = {
+local DEFAULTS = {
 	char = {
 		Item = '',
-		Goal = 0,
-		GoalNotification = 0
+		Goal = '',
+		GoalNotification = 0,
+		ShowItemName = 1
 	}
 };
-local options = {
-	name = ADDON_NAME,
-	handler = TitanFarmBuddy,
-	type = 'group',
-	args = {
-		info_version = {
-			type		= 'description',
-			name		= L['Version'] .. ': ' .. ADDON_VERSION,
-			order		= 0,
-		},
-		info_author = {
-			type		= 'description',
-			name		= L['Author'] .. ': ' .. ADDON_AUTHOR,
-			order		= 10,
-		},
-		header_general = {
-			type		= 'header',
-			name		= L['General Options'],
-			order		= 20,
-		},
-		item_count = {
-			type     = 'input',
-			name     = L['Item to Track'],
-			desc     = L['The name of the item to track'],
-			get      = 'GetItem',
-			set      = 'SetItem',
-			usage    = L['Enter the name of an item or CTRL + Click an item from your inventory.'],
-			width    = 'double',
-			order    = 30,
-		},
-		space_1 = {
-			type     = 'description',
-			name     = '',
-			order    = 40,
-		},
-		goal = {
-			type     = 'input',
-			name     = L['Quantity for Alert'],
-			desc     = L['The goal quantity for the tracked item to show a finish notification.'],
-			get      = 'GetGoal',
-			set      = 'SetGoal',
-			usage    = L['An number for your farming goal.'],
-			width    = 'double',
-			order    = 50,
-		},
-		space_2 = {
-			type     = 'description',
-			name     = '',
-			order    = 60,
-		},
-		item_notification_status = {
-			type	 = 'toggle',
-			name     = L['Notification if quantity has reached'],
-			desc     = L['Shows a notification if the item quantity has reached.'],
-			get	     = 'GetNotificationStatus',
-			set	     = 'SetNotificationStatus',
-			width	 = 'full',
-			order	 = 70,
-		},
-		header_actions = {
-			type	 = 'header',
-			name	 = L['Actions'],
-			order	 = 80,
-		},
-		item_reset = {
-			type	 = 'execute',
-			name     = L['Reset'],
-			desc     = L['Resets the tracked item.'],
-			func	 = 'ResetConfig',
-			width    = 'double',
-			order	 = 90,
-		},
-		space_3 = {
-			type     = "description",
-			name     = "",
-			order    = 100,
-		},
-		item_test_alert = {
-			type	 = 'execute',
-			name     = L['Test Notification'],
-			desc     = L['Triggers a test for the finish notification.'],
-			func	 = 'TestNotification',
-			width    = 'double',
-			order	 = 110,
-		}
-	}
-}
 
 -- **************************************************************************
 -- NAME : TitanFarmBuddy:OnInitialize()
@@ -112,10 +24,121 @@ local options = {
 -- **************************************************************************
 function TitanFarmBuddy:OnInitialize()
 
-	self.db = LibStub('AceDB-3.0'):New('TitanFarmBuddyDB', defaults, true)
+	self.db = LibStub('AceDB-3.0'):New('TitanFarmBuddyDB', DEFAULTS, true)
 
-    LibStub("AceConfig-3.0"):RegisterOptionsTable(ADDON_NAME, options);
-    LibStub("AceConfigDialog-3.0"):AddToBlizOptions(ADDON_NAME);
+    LibStub('AceConfig-3.0'):RegisterOptionsTable(ADDON_NAME, TitanFarmBuddy:GetConfigOption());
+    LibStub('AceConfigDialog-3.0'):AddToBlizOptions(ADDON_NAME);
+end
+
+-- **************************************************************************
+-- NAME : TitanFarmBuddy:GetConfigOption()
+-- DESC : Gets the configuration array for the AceConfig lib
+-- **************************************************************************
+function TitanFarmBuddy:GetConfigOption()
+	return {
+		name = ADDON_NAME,
+		handler = TitanFarmBuddy,
+		type = 'group',
+		args = {
+			info_version = {
+				type		= 'description',
+				name		= L['Version'] .. ': ' .. ADDON_VERSION,
+				order		= 0,
+			},
+			info_author = {
+				type		= 'description',
+				name		= L['Author'] .. ': ' .. GetAddOnMetadata('TitanFarmBuddy', 'Author'),
+				order		= 10,
+			},
+			header_general = {
+				type		= 'header',
+				name		= L['General Options'],
+				order		= 20,
+			},
+			item_count = {
+				type     = 'input',
+				name     = L['Item to Track'],
+				desc     = L['The name of the item to track'],
+				get      = 'GetItem',
+				set      = 'SetItem',
+				validate = 'ValidateItem',
+				usage    = L['Enter the name of an item or CTRL + Click an item from your inventory.'],
+				width    = 'double',
+				order    = 30,
+			},
+			space_1 = {
+				type     = 'description',
+				name     = '',
+				order    = 40,
+			},
+			goal = {
+				type     = 'input',
+				name     = L['Quantity for Alert'],
+				desc     = L['The goal quantity for the tracked item to show a finish notification.'],
+				get      = 'GetGoal',
+				set      = 'SetGoal',
+				usage    = L['An number for your farming goal.'],
+				width    = 'double',
+				order    = 50,
+			},
+			space_2 = {
+				type     = 'description',
+				name     = '',
+				order    = 60,
+			},
+			item_show_item_name = {
+				type	   = 'toggle',
+				name     = L['Show item name'],
+				desc     = L['Show the item name on the Titan Bar.'],
+				get	     = 'GetShowItemName',
+				set	     = 'SetShowItemName',
+				width	   = 'full',
+				order	   = 70,
+			},
+			space_3 = {
+				type     = 'description',
+				name     = '',
+				order    = 80,
+			},
+			item_notification_status = {
+				type	   = 'toggle',
+				name     = L['Notification if quantity has reached'],
+				desc     = L['Shows a notification if the item quantity has reached.'],
+				get	     = 'GetNotificationStatus',
+				set	     = 'SetNotificationStatus',
+				width	   = 'full',
+				order	   = 90,
+			},
+			header_actions = {
+				type	   = 'header',
+				name	   = L['Actions'],
+				order	   = 100,
+			},
+			item_reset = {
+				type	   = 'execute',
+				name     = L['Reset'],
+				desc     = L['Resets the tracked item.'],
+				func	   = 'ResetConfig',
+				width    = 'double',
+				order	   = 110,
+			},
+			space_3 = {
+				type     = 'description',
+				name     = '',
+				order    = 120,
+			},
+			item_test_alert = {
+				type	   = 'execute',
+				name     = L['Test Notification'],
+				desc     = L['Triggers a test for the finish notification.'],
+				func	   = 'TestNotification',
+				width    = 'double',
+				order	   = 130,
+			}
+
+			-- TODO: Notification Sound select
+		}
+	}
 end
 
 -- **************************************************************************
@@ -132,7 +155,7 @@ function TitanPanelFarmBuddyButton_OnLoad(self)
 		tooltipTitle = ADDON_NAME,
 		tooltipTextFunction = 'TitanPanelFarmBuddyButton_GetTooltipText',
 		icon = 'Interface\\AddOns\\TitanFarmBuddy\\TitanFarmBuddy',
-		iconWidth = 16,
+		iconWidth = 0,
 		controlVariables = {
 			ShowIcon = true,
 			ShowLabelText = true,
@@ -156,14 +179,18 @@ end
 -- **************************************************************************
 function TitanPanelFarmBuddyButton_GetButtonText(id)
 
-	local itemName, itemLink = GetItemInfo(self.db.char.Item)
+	-- TODO: Get show icon status from Titan Panel
+	-- self.registry['controlVariables'].ShowIcon
 
-	TitanFarmBuddy:print(self.db.char.Item)
+	-- TODO: Refactor and handle options
+	-- TODO: Color item count
 
+	local itemName, itemLink = GetItemInfo(TitanFarmBuddy.db.char.Item)
 	local iconFileDataID = GetItemIcon(itemLink)
-	local label = "|T" .. iconFileDataID .. ":16:16:0:-2|t"
+	local itemCount = GetItemCount(itemLink)
+	local label = '|T' .. iconFileDataID .. ':16:16:0:-2|t'
 
-	return label .. ' 10 ' .. itemName
+	return label .. ' ' .. itemCount .. ' ' .. itemName
 end
 
 function TitanPanelFarmBuddyButton_OnClick(self, button)
@@ -204,12 +231,28 @@ function TitanPanelFarmBuddyButton_OnHide()
 end
 
 -- **************************************************************************
+-- NAME : TitanFarmBuddy:ValidateItem()
+-- DESC : Checks if the entered item name is valid
+-- **************************************************************************
+function TitanFarmBuddy:ValidateItem(info, input)
+
+	local _, itemLink = GetItemInfo(input)
+
+	if itemLink ~= nil then
+		return true
+	end
+
+	TitanFarmBuddy:Print(L['The item does not exists.']);
+	return false
+end
+
+-- **************************************************************************
 -- NAME : TitanFarmBuddy:SetItem()
 -- DESC : Sets the item
 -- **************************************************************************
 function TitanFarmBuddy:SetItem(info, input)
    self.db.char.Item = input
-   TitanPanelButton_UpdateButton(ID)
+   TitanPanelButton_UpdateButton(TITAN_FARM_BUDDY_ID)
 end
 
 -- **************************************************************************
@@ -226,7 +269,7 @@ end
 -- **************************************************************************
 function TitanFarmBuddy:SetGoal(info, input)
    self.db.char.Goal = tonumber(input)
-   TitanPanelButton_UpdateButton(ID)
+   TitanPanelButton_UpdateButton(TITAN_FARM_BUDDY_ID)
 end
 
 -- **************************************************************************
@@ -237,19 +280,54 @@ function TitanFarmBuddy:GetGoal()
    return tostring(self.db.char.Goal)
 end
 
-function TitanFarmBuddy:ResetConfig()
-	self.db.char.Item = ''
-	self.db.char.Goal = ''
-end
-
-function TitanFarmBuddy:TestNotification()
-	TitanFarmBuddy:print('Test Test');
-end
-
+-- **************************************************************************
+-- NAME : TitanFarmBuddy:SetNotificationStatus()
+-- DESC : Sets the notification status
+-- **************************************************************************
 function TitanFarmBuddy:SetNotificationStatus(info, input)
 	self.db.char.GoalNotification = input
 end
 
+-- **************************************************************************
+-- NAME : TitanFarmBuddy:GetNotificationStatus()
+-- DESC : Gets the notification status
+-- **************************************************************************
 function TitanFarmBuddy:GetNotificationStatus()
 	return self.db.char.GoalNotification
+end
+
+-- **************************************************************************
+-- NAME : TitanFarmBuddy:SetShowItemName()
+-- DESC : Sets the show item name status
+-- **************************************************************************
+function TitanFarmBuddy:SetShowItemName(info, input)
+	self.db.char.ShowItemName = input
+end
+
+-- **************************************************************************
+-- NAME : TitanFarmBuddy:GetShowItemName()
+-- DESC : Gets the show item name status
+-- **************************************************************************
+function TitanFarmBuddy:GetShowItemName()
+	return self.db.char.ShowItemName
+end
+
+-- **************************************************************************
+-- NAME : TitanFarmBuddy:ResetConfig()
+-- DESC : Resets the saved config to the default values
+-- **************************************************************************
+function TitanFarmBuddy:ResetConfig()
+	self.db.char.Item = DEFAULTS.char.Item
+	self.db.char.Goal = DEFAULTS.char.Goal
+	self.db.char.GoalNotification = DEFAULTS.char.GoalNotification
+	self.db.char.ShowItemName = DEFAULTS.char.ShowItemName
+end
+
+-- **************************************************************************
+-- NAME : TitanFarmBuddy:TestNotification()
+-- DESC : Raises a test notification
+-- **************************************************************************
+function TitanFarmBuddy:TestNotification()
+	-- TODO: Call notification function
+	TitanFarmBuddy:Print('Hello, World!')
 end
