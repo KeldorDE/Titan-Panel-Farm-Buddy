dc122497e4422617f32d3a43fbd0f4b3a3714a17
André Evertz <andre.evertz@devil-coding.de>
2017-08-26 18:03:50 +0200

    Added all languages Changed get translation script

--- locale/Localization.frFR.lua
+++ locale/Localization.frFR.lua
@@ -1,65 +1,126 @@
-local L = LibStub('AceLocale-3.0'):NewLocale('Titan', 'deDE')
+local L = LibStub('AceLocale-3.0'):NewLocale('Titan', 'frFR', false)
 if not L then return end
 
 L = L or {}
+--Translation missing 
 L["FARM_BUDDY_ACTIONS"] = "Actions"
+--Translation missing 
 L["FARM_BUDDY_ALERT_COUNT"] = "Quantity for Alert"
+--Translation missing 
 L["FARM_BUDDY_ALERT_COUNT_USAGE"] = "An quantity for your farming goal."
+--Translation missing 
 L["FARM_BUDDY_ANCHOR_HELP_TEXT"] = "Hold left mouse button to move. Right click to close."
+--Translation missing 
 L["FARM_BUDDY_AUTHOR"] = "Author"
+--Translation missing 
 L["FARM_BUDDY_BANK"] = "Bank"
+--Translation missing 
 L["FARM_BUDDY_COMMAND_GOAL_ARGS"] = "Quantity"
+--Translation missing 
 L["FARM_BUDDY_COMMAND_GOAL_DESC"] = "Sets the goal quantity."
+--Translation missing 
 L["FARM_BUDDY_COMMAND_GOAL_PARAM_MISSING"] = "You have to set a quantity as second parameter."
+--Translation missing 
 L["FARM_BUDDY_COMMAND_HELP_DESC"] = "Prints this information."
+--Translation missing 
 L["FARM_BUDDY_COMMAND_LIST"] = "List of Chat Commands"
+--Translation missing 
 L["FARM_BUDDY_COMMAND_RESET_DESC"] = "Resets Farm Buddy to it's default settings."
+--Translation missing 
 L["FARM_BUDDY_COMMAND_TRACK_ARGS"] = "Item Name|Item Link"
+--Translation missing 
 L["FARM_BUDDY_COMMAND_TRACK_DESC"] = "Sets the tracked item."
+--Translation missing 
 L["FARM_BUDDY_COMMAND_VERSION_DESC"] = "Show the current used Farm Buddy Version."
+--Translation missing 
 L["FARM_BUDDY_CONFIG_RESET_MSG"] = "The configuration has been set back to the defaults."
+--Translation missing 
 L["FARM_BUDDY_GOAL_SET"] = "The goal quantity has been set."
+--Translation missing 
 L["FARM_BUDDY_INCLUDE_BANK"] = "Include items in your bank"
+--Translation missing 
 L["FARM_BUDDY_INCLUDE_BANK_DESC"] = "If enabled items in your bank are included when counting the farmed item."
+--Translation missing 
 L["FARM_BUDDY_INVALID_NUMBER"] = "The entered number is not a valid number."
+--Translation missing 
 L["FARM_BUDDY_INVENTORY"] = "Inventory"
+--Translation missing 
 L["FARM_BUDDY_ITEM"] = "Item"
+--Translation missing 
 L["FARM_BUDDY_ITEM_NOT_EXISTS"] = "The item does not exists."
+--Translation missing 
 L["FARM_BUDDY_ITEM_SET_MSG"] = "!itemName! is now your tracked item!"
+--Translation missing 
 L["FARM_BUDDY_ITEM_TO_TRACK_DESC"] = "The name of the item to track"
+--Translation missing 
 L["FARM_BUDDY_ITEM_TO_TRACK_USAGE"] = "Enter the name of an item or CTRL + Click an item from your inventory. Please note: The item have to be in your World of Warcraft Data Cache otherwise the item is not known to the AddOn API functions."
+--Translation missing 
 L["FARM_BUDDY_ITEMS"] = "Items"
+--Translation missing 
 L["FARM_BUDDY_MOVE_NOTIFICATION"] = "Change Notification Position"
+--Translation missing 
 L["FARM_BUDDY_MOVE_NOTIFICATION_DESC"] = "Change the Position of the Notification Frame."
+--Translation missing 
 L["FARM_BUDDY_NO_GOAL"] = "No goal defined"
+--Translation missing 
 L["FARM_BUDDY_NO_ITEM_TRACKED"] = "You have no item for tracking selected."
+--Translation missing 
 L["FARM_BUDDY_NOTIFICATION"] = "Notification if quantity has reached"
+--Translation missing 
 L["FARM_BUDDY_NOTIFICATION_DEMO_ITEM_NAME"] = "Hearthstone"
+--Translation missing 
 L["FARM_BUDDY_NOTIFICATION_DESC"] = "Shows a notification if the item quantity has reached."
+--Translation missing 
 L["FARM_BUDDY_NOTIFICATIONS"] = "Notifications"
+--Translation missing 
 L["FARM_BUDDY_PLAY_NOTIFICATION_DISPLAY_DURATION"] = "Notification Display Duration"
+--Translation missing 
 L["FARM_BUDDY_PLAY_NOTIFICATION_DISPLAY_DURATION_DESC"] = "The Notification Display Duration in seconds."
+--Translation missing 
 L["FARM_BUDDY_PLAY_NOTIFICATION_SOUND"] = "Play Notification Sound"
+--Translation missing 
 L["FARM_BUDDY_PLAY_NOTIFICATION_SOUND_DESC"] = "Play a notification sound file if the farm goal has reached."
+--Translation missing 
 L["FARM_BUDDY_QUANTITY"] = "Quantity"
+--Translation missing 
 L["FARM_BUDDY_RESET"] = "Reset"
+--Translation missing 
 L["FARM_BUDDY_RESET_DESC"] = "Resets the tracked item."
+--Translation missing 
 L["FARM_BUDDY_SETTINGS"] = "Common"
+--Translation missing 
 L["FARM_BUDDY_SHOW_COLORED_TEXT"] = "Show Colored Text"
+--Translation missing 
 L["FARM_BUDDY_SHOW_COLORED_TEXT_DESC"] = "Show the item count as colored text on the Titan Bar."
+--Translation missing 
 L["FARM_BUDDY_SHOW_GOAL"] = "Show Goal on Titan Bar"
+--Translation missing 
 L["FARM_BUDDY_SHOW_GOAL_DESC"] = "Show the goal quantity on the Titan Bar if a goal is defined."
+--Translation missing 
 L["FARM_BUDDY_SHOW_ICON"] = "Show icon"
+--Translation missing 
 L["FARM_BUDDY_SHOW_ICON_DESC"] = "Show the item icon on the Titan Bar."
+--Translation missing 
 L["FARM_BUDDY_SHOW_NAME"] = "Show item name"
+--Translation missing 
 L["FARM_BUDDY_SHOW_NAME_DESC"] = "Show the item name on the Titan Bar."
+--Translation missing 
 L["FARM_BUDDY_SUMMARY"] = "Summary"
+--Translation missing 
 L["FARM_BUDDY_TEST_NOTIFICATION"] = "Test Notification"
+--Translation missing 
 L["FARM_BUDDY_TEST_NOTIFICATION_DESC"] = "Triggers a test for the finish notification."
+--Translation missing 
 L["FARM_BUDDY_TOOLTIP_DESC"] = "Left click to open the Settings."
+--Translation missing 
 L["FARM_BUDDY_TOOLTIP_MODIFIER"] = "Alt + Right click on an item in your Bag to set item."
+--Translation missing 
 L["FARM_BUDDY_TOTAL"] = "Total"
+--Translation missing 
 L["FARM_BUDDY_TRACK_ITEM_PARAM_MISSING"] = "You have to set an Item Name or Item Link as second parameter."
+--Translation missing 
 L["FARM_BUDDY_TRACKING_DESC"] = "You can track up to 4 items at once and select one item that is shown in the titan bar the other items are shown in the tooltip of Farm Buddy."
+--Translation missing 
 L["FARM_BUDDY_VERSION"] = "Version"
+--Translation missing 
 L["TITAN_BUDDY_NOTIFICATION_SOUND"] = "Notification Sound"
--- scripts/get_translation.sh
+++ scripts/get_translation.sh
@@ -2,6 +2,7 @@
 
 API_PROJECT_ID="274538"
 API_URL="https://wow.curseforge.com/api/projects/${API_PROJECT_ID}/localization/export"
+LANGS=( "enUS" "enGB" "deDE" "frFR" "esES" "itIT" "ruRU" "koKR" "zhCN" "zhTW" "esMX" "ptBR" )
 
 ROOT=$(pwd)
 if [ "${ROOT: -8}" == "/scripts" ]; then
@@ -10,7 +11,7 @@ fi
 
 INCLUDE_API_FILE="${ROOT}/scripts/api.token.sh"
 
-if [ ! -f "${INCLUDE_API_FILE}" ];then
+if [ ! -f "${INCLUDE_API_FILE}" ]; then
   echo "\033[1;31mNo ${INCLUDE_API_FILE} file found!\033[m"
   exit 1
 fi
@@ -23,26 +24,27 @@ if [ -z ${API_TOKEN} ]; then
 fi
 
 LOCALE_PATH="${ROOT}/locale"
-LOCALE_EN="${LOCALE_PATH}/Localization.lua"
-LOCALE_DE="${LOCALE_PATH}/Localization.DE.lua"
 
-echo "Fetching translation for enUS"
-RESP_EN=$($(which curl) --header "X-Api-Token: ${API_TOKEN}" --silent "${API_URL}?lang=enUS")
+for i in "${LANGS[@]}"
+do
 
-echo "Fetching translation for deDE"
-RESP_DE=$($(which curl) --header "X-Api-Token: ${API_TOKEN}" --silent "${API_URL}?lang=deDE")
+  if [ $i == "enUS" ]; then
+    LOCALE_DEFAULT="true"
+  else
+    LOCALE_DEFAULT="false"
+  fi
 
-echo "Writing ${LOCALE_EN}"
-echo "local L = LibStub('AceLocale-3.0'):NewLocale('Titan', 'deDE')" > "${LOCALE_EN}"
-echo "if not L then return end" >> "${LOCALE_EN}"
-echo "" >> "${LOCALE_EN}"
-echo "${RESP_EN}" >> "${LOCALE_EN}"
+  LOCALE_FILE="${LOCALE_PATH}/Localization.$i.lua"
 
-echo "Writing ${LOCALE_DE}"
-echo "local L = LibStub('AceLocale-3.0'):NewLocale('Titan', 'deDE')" > "${LOCALE_DE}"
-echo "if not L then return end" >> "${LOCALE_DE}"
-echo "" >> "${LOCALE_DE}"
-echo "${RESP_DE}" >> "${LOCALE_DE}"
+  echo "Fetching translation for $i"
+  RESP=$($(which curl) --header "X-Api-Token: ${API_TOKEN}" --silent "${API_URL}?lang=$i")
+
+  echo "Writing ${LOCALE_FILE}"
+  echo "local L = LibStub('AceLocale-3.0'):NewLocale('Titan', '$i', ${LOCALE_DEFAULT})" > "${LOCALE_FILE}"
+  echo "if not L then return end" >> "${LOCALE_FILE}"
+  echo "" >> "${LOCALE_FILE}"
+  echo "${RESP}" >> "${LOCALE_FILE}"
+done
 
 echo -e "\033[1;32m[OK] All trnalsations has been updated\033[m"
 
