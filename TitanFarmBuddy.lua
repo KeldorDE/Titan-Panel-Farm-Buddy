6c4685da63ae24d7703b17f7721fcefb5805eeb1
André Evertz <andre.evertz@devil-coding.de>
2026-08-04 10:30:38 +0200

    refactor: remove debug print statement in track command of TitanFarmBuddy.lua

--- TitanFarmBuddy.lua
+++ TitanFarmBuddy.lua
@@ -1629,7 +1629,6 @@ function TitanFarmBuddy:ChatCommand(input)
     elseif cmd == 'track' then
 
         if value then
-            print(arg1)
             local itemInfo = self:GetItemInfo(arg1)
             if itemInfo then
                 local index = tonumber(value)
